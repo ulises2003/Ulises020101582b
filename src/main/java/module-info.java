b8acbcdc12500dcc4014a107a92e3b8f0abc7dd3
Ulises <020101582b@uandina.edu.pe>
2022-08-08 13:54:35 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.ulises020101582b {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.ulises020101582b to javafx.fxml;
+    exports com.example.ulises020101582b;
+}
